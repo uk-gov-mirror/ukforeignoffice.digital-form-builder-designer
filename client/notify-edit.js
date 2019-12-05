dcfebcb27a0016c99ef9dd003993e0d0cab69bf6
Jen <jen+git@cautionyourblast.com>
2019-12-05 18:19:03 +0000

    email field

--- client/notify-edit.js
+++ client/notify-edit.js
@@ -22,9 +22,11 @@ class NotifyEdit extends React.Component {
     // Items
     const personalisation = formData.getAll('personalisation').map(t => t.trim())
     const templateId = formData.get('template-id')
+    const emailField = formData.get('email-field')
     copy.notify = {
       personalisation,
-      templateId
+      templateId,
+      emailField
     }
 
     data.save(copy)
@@ -39,7 +41,8 @@ class NotifyEdit extends React.Component {
   render () {
     const { data } = this.props
     const { notify, conditions } = data
-    const { templateId, personalisation } = notify
+    const { templateId, personalisation, emailField } = notify
+    const values = [...conditions.map(condition => condition.name), ...this.usableKeys]
 
     return (
       <div className='govuk-body'>
@@ -52,8 +55,14 @@ class NotifyEdit extends React.Component {
                      type='text' required defaultValue={templateId}
                      onBlur={this.onBlur} step='any'/>
             </div>
+          <div className="govuk-form-group">
+            <label className='govuk-label' htmlFor='email-field'>Email field</label>
+            <select className='govuk-select' id='email-field' name='email-field' defaultValue={emailField} required>
+              {values.map((value, i)=> (<option key={value + i} value={value} onBlur={this.onBlur}>{value}</option>))}
+            </select>
+          </div>
 
-          <NotifyItems items={personalisation} values={[...conditions.map(condition => condition.name), ...this.usableKeys]}  />
+          <NotifyItems items={personalisation} values={values}  />
 
           <button className='govuk-button' type='submit'>Save</button>
         </form>
